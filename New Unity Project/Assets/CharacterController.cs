8fd1f0d4c950e09c9e6674270236ddf380b48be8
Deeone78 <deeyimark@gmail.com>
2022-11-17 15:09:38 +0000

    Finished lesson 3

--- New Unity Project/Assets/CharacterController.cs
+++ New Unity Project/Assets/CharacterController.cs
@@ -7,24 +7,54 @@ public class CharacterController : MonoBehaviour
     // Start is called before the first frame update
     GameObject cam;
     Rigidbody myRigidbody;
-
-
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 3000.0f; 
+    public float maxSprint = 5.0f;
+    float sprintTimer;
+    
     void Start()
     {
         cam = GameObject.Find("Main Camera");
         myRigidbody = GetComponent<Rigidbody>();
+        sprintTimer = maxSprint;
 
     }
 
     // Update is called once per frame
     float rotation = 0.0f;
-    float maxSpeed = 10f;
+    public float normalSpeed = 10f;
+    public float maxSpeed;
+    public float sprintSpeed = 20f; 
+
     float camRoatation = 0.0f;
     float rotaiotionSpeed = 5.0f;
     float camRotationSpeed = 5.0f;
+    
 
     void Update()
     {
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up* jumpForce);
+
+        }
+        isOnGround = Physics.CheckSphere(groundChecker .transform.position, 0.1f, groundLayer);
+        
+        if (Input.GetKey(KeyCode.LeftShift)&& sprintTimer > 0.0f)
+        {
+            maxSpeed = sprintSpeed;
+            sprintTimer = sprintTimer - Time.deltaTime;
+        }else
+        {   
+            maxSpeed = normalSpeed;
+            if (Input.GetKey(KeyCode.LeftShift)==false){
+                sprintTimer = sprintTimer + Time.deltaTime;
+            }
+
+        }
+        sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
 
         Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
         newVelocity += transform.right * Input.GetAxis("Horizontal")* maxSpeed ;
