848d8c62e1153f09387e2ae0162a2c0ae42bd824
Deeone78 <deeyimark@gmail.com>
2022-11-28 10:47:34 +0000

    everything has been fixed

---
+++ Assets/scrpits/Pickup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Destroy(gameObject);
+    }
+  
+  
+  
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
